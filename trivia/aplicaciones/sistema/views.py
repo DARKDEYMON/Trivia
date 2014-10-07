e36b6e36e3c2ffd3fec8b674de8e7e6351eab1e6
darkdeymon <darkdeymon04@gmail.com>
2014-10-07 17:16:50 -0400

    Version 2

--- trivia/aplicaciones/sistema/views.py
+++ trivia/aplicaciones/sistema/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,render_to_response
+from django.template import RequestContext
+from trivia.aplicaciones.sistema.forms import *
 
 # Create your views here.
+def usuario_login(request):
+	name="login"
+	return render_to_response("principales/index_login.html",{"name":name,"login":form_usuario_for_final_user()},RequestContext(request))
